fd1bbd5ad4b84341aecc5936964c2d9663242e48
vrush-anvit <vrushalijagtap099@gmail.com>
2025-11-12 21:43:59 +0530

    pacinkg added

--- pcode.py
+++ pcode.py
@@ -1,5 +1,11 @@
-s=input("enter string")
-i=0
-for x in s:
-    print("positive string {} and negative string {} is {}".format(i,i-len(s),x))
-    i=i+1
+# s=input("enter string")
+# i=0
+# for x in s:
+#     print("positive string {} and negative string {} is {}".format(i,i-len(s),x))
+#     i=i+1
+
+a=10
+b=20
+c=30
+t=a,b,c
+print(t)
